76820d6eb93297831853dece8864e4a47dde19d8
Lorwen <ltranngocdai@gmail.com>
2016-05-13 05:40:10 -0400

    commit models/functions.php

---
+++ models/functions.php
@@ -0,0 +1,26 @@
+<?php
+// Connexion à la BDD
+$dbh = new PDO('mysql:host=localhost;dbname=les lettres perdues', 'root', '');
+
+// Renvoie la liste des aticles sélectionnés
+function getArticles()
+{
+  // Connexion à la BDD
+  global $dbh;
+  // Requête de sélection
+  $articles = $dbh->query("SELECT id, date, title, content FROM article ORDER BY date DESC") or die(print_r($dbh->errorInfo(), ture));
+  // Renvoie de la valeur
+  return $articles;
+}
+
+function getLogs() {
+  // Connexion à la BDD
+  global $dbh;
+  // Sécurisation des entrées
+  include("includes/secured_form.php");
+  // Requête pour récupérer les informations de connexion
+  $req = $dbh-> prepare('SELECT id, username, password FROM user WHERE username = ? AND password =  ?');
+  $logs = array($_POST['pseudo'], $_POST['password']);
+
+  return $logs;
+}
